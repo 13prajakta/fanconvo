fb6ea6a3e14ee0d45dd142b9941838e6034fc334
13prajakta <13psathwane@gmail.com>
2021-08-25 18:48:01 +0530

    hello

---
+++ src/signup/Footer.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import '../assets/Signup.scss';
+
+function Footer() {
+    return (
+        <div>
+            <div className="container-fluid background ">
+                <div class="main footer">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                    <img className="logo img-responsive" src="logo.png" />
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="nav navbar-nav ml-auto anchore">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">How Fanconovo Works?</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"> Terms Of Use</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"> Contact Us</a>
+                        </li>
+                        
+                        </ul> 
+                        
+                    </div>
+                    </nav>
+                </div>
+                <p>@ 2021 Fonconovo</p>
+            </div>
+        </div>
+    )
+}
+export default Footer
---
+++ src/util/validation.js
@@ -0,0 +1,38 @@
+export const validate=(data)=>{
+    let error={}
+    var namereg = /^[a-zA-Z_ ]*$/;
+     var emailreg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(data.fname && data.lname && data.email && data.uname && data.time && data.password){
+    if(!data.fname.value){
+        error.fname="*First Name is required"
+    }
+    else if(!namereg.test(data.fname.value)){
+        error.fname="*only alphabates"
+    }
+     if(!data.lname.value){
+        error.lname="*Last Name is required"
+    }
+    else if(!namereg.test(data.lname.value)){
+        error.lname="*only alphabates"
+    }
+     if(!data.uname.value){
+        error.uname="*User Name is required"
+    }
+    else if(!namereg.test(data.uname.value)){
+        error.uname="*only alphabates"
+    }
+    if(!data.email.value){
+        error.email="*email is required"
+    }
+    else if(!emailreg.test(data.email.value)){
+        error.email="*invalid email type"
+    }
+    if(data.time.value==" "){
+        error.time="*time is required"
+    }
+    if(!data.password.value){
+        error.password="*Password is required"
+    }
+    return error;
+}
+}
