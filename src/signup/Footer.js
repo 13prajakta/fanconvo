b18e6a234985720ec4dc27f6f01bb88df42d46b1
13prajakta <13psathwane@gmail.com>
2021-08-26 13:57:39 +0530

    ok

--- src/signup/Footer.js
+++ src/signup/Footer.js
@@ -7,11 +7,11 @@ function Footer() {
             <div className="container-fluid background ">
                 <div class="main footer">
                     <nav class="navbar navbar-expand-lg navbar-light">
-                        <img className="logo img-responsive" src="logo.png" />
+                        <img className="logo img-responsive text-center" src="logo.png" />
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
-                        <div class="collapse navbar-collapse" id="navbarNav">
+                        <div class="" id="navbarNav">
                             <ul class="nav navbar-nav ml-auto anchore">
                                 <li class="nav-item">
                                     <a class="nav-link" href="#">How Fanconovo Works?</a>
--- src/signup/Nav.js
+++ src/signup/Nav.js
@@ -4,22 +4,21 @@ function Nav() {
     return (
         <>
             <nav class="navbar navbar-expand-lg navbar-light">
-                        <img className="logo img-responsive" src="logo.png" />
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarNav">
-                            <ul class="nav navbar-nav ml-auto anchore">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Signup</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Login</a>
-                                </li>
-
-                            </ul>
-                        </div>
-                    </nav>
+                <img className="logo img-responsive" src="logo.png" />
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="nav navbar-nav ml-auto anchore">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Signup</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </>
     )
 }
