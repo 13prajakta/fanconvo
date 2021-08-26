576ac94bad999b6cfad11c03c6f2baba85ad6888
13prajakta <13psathwane@gmail.com>
2021-08-26 13:53:08 +0530

    ok

--- src/signup/Signup.js
+++ src/signup/Signup.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../assets/Signup.scss';
 import Footer from './Footer.js'
+import Nav from './Nav.js'
 import { useState } from 'react'
 import { validate } from '../util/validation.js';
 import axios from 'axios'
@@ -89,23 +90,7 @@ function Signup() {
         <>
             <div className="container-fluid background">
                 <div class="main">
-                    <nav class="navbar navbar-expand-lg navbar-light">
-                        <img className="logo img-responsive" src="logo.png" />
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarNav">
-                            <ul class="nav navbar-nav ml-auto anchore">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Signup</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Login</a>
-                                </li>
-
-                            </ul>
-                        </div>
-                    </nav>
+                    <Nav />
                     <div className="tag">
                         <p>A MarketPlace For Conversation, mentorship and performance</p>
                         <span>Search New Talent</span>
