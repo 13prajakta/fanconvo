677998933dc1423aae33792965655c6da5c6a4d0
13prajakta <13psathwane@gmail.com>
2021-08-26 13:34:00 +0530

    ok

--- src/signup/Signup.js
+++ src/signup/Signup.js
@@ -110,7 +110,7 @@ function Signup() {
                     <p>A MarketPlace For Conversation, mentorship and performance</p>
                     <span>Search New Talent</span>
                      <form class="form-inline">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <input class="form-control searchh mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
                         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                     </form>
                     </div>
